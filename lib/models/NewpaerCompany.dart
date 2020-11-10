be75c4bfbfbb423ae6cafc787ad0d44b983c4491
Habib <seremehabibf@gmail.com>
2020-11-10 19:24:26 +0000

    fireBase config: User and Company model

---
+++ lib/models/NewpaerCompany.dart
@@ -0,0 +1,58 @@
+/// represent de company info
+
+class NewspaperCompany{
+  /// VARIABLES
+  //  ID
+  String _id;
+  String get id => _id;
+  set id(String value) {
+    _id = value;
+  }
+  // LOGO URL
+  String _logoURL;
+  String get logoURL => _logoURL;
+  set logoURL(String value) {
+    _logoURL = value;
+  }
+  // NAME
+  String _name;
+  String get name => _name;
+  set name(String value) {
+    _name = value;
+  }
+  // COUNTRY
+  String _country;
+  String get country => _country;
+  set country(String value) {
+    _country = value;
+  }
+  // CITY
+  String _city;
+  String get city => _city;
+  set city(String value) {
+    _city = value;
+  }
+  // MAIL
+  List<String> _mail = List();
+  List<String> get mail => _mail;
+  set mail(List<String> value) {
+    _mail = value;
+  }
+  // NEWSPAPER
+  List<String> _newspapers = List();
+  List<String> get newspapers => _newspapers;
+  set newspapers(List<String> value) {
+    _newspapers = value;
+  }
+  // PHONE NUMBER
+  List<num> _phoneNumber = List();
+  List<num> get phoneNumber => _phoneNumber;
+  set phoneNumber(List<num> value) {
+    _phoneNumber = value;
+  } //firebase firestore reference
+
+  final String REF_FIREBASE_FIRESTORE = 'Company';
+
+  NewspaperCompany();
+
+}
--- lib/models/ToxNewsUsers.dart
+++ lib/models/ToxNewsUsers.dart
@@ -3,54 +3,57 @@
 class ToxNewsUsers{
 
   // variables
+  // ID
   String _id;
   String get id => _id;
   set id(String value) {
     _id = value;
   }
-
+  //MAIL
   String _mail;
   String get mail => _mail;
   set mail(String value) {
     _mail = value;
   }
-
+  //NAME
   String _name;
   String get name => _name;
   set name(String value) {
     _name = value;
   }
-
+  // NUMBER
   String _number;
   String get number => _number;
   set number(String value) {
     _number = value;
   }
-
+  // IS SUBSCRIBER
   bool _subscriber;
   bool get subscriber => _subscriber;
   set subscriber(bool value) {
     _subscriber = value;
   }
-
+  /// NEWSPAPER
+  // OWN
   List<String> _newsPaperOwned = List();
   List<String> get newsPaperOwned => _newsPaperOwned;
   set newsPaperOwned(List<String> value) {
     _newsPaperOwned = value;
   }
-
+  // SUBSCRIBE
   List<String> _newsPaperSubscribed = List();
   List<String> get newsPaperSubscribed => _newsPaperSubscribed;
   set newsPaperSubscribed(List<String> value) {
     _newsPaperSubscribed = value;
   }
-
+  /// FAVORITE
+  // COMPANY
   List<String> _favoriteCompanies = List();
   List<String> get favoriteCompanies => _favoriteCompanies;
   set favoriteCompanies(List<String> value) {
     _favoriteCompanies = value;
   }
-
+  // CATEGORY
   List<String> _favoriteCategories = List();
   List<String> get favoriteCategories => _favoriteCategories;
   set favoriteCategories(List<String> value) {
